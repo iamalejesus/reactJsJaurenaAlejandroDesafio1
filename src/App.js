9155b72dd4cc720e7cd13b258b4ae6f749712cba
iamale <iamalejandrojesus@gmail.com>
2022-04-25 19:22:26 -0300

    agregando carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/container/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer';
 import Footer from './components/Footer';
+import Carousel from './components/Carousel';
 
 function App() {
   return ( 
     <>
       <NavBar/>
+      <Carousel/>
       <ItemListContainer/>
       <Footer/>
     </> 
